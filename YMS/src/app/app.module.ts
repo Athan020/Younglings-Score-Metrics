e41d7f86360b1bd875564889cf9becd566e16325
MC-Tortuga <malcolmcarelse@gmail.com>
2018-02-05 09:42:55 +0200

    Set up Dashboard Components

--- YMS/src/app/app.module.ts
+++ YMS/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TeamMemberComponent } from './components/dashboard/team-member/team-member.component';
+import { TeamLeaderComponent } from './components/dashboard/team-leader/team-leader.component';
+import { ManagerComponent } from './components/dashboard/manager/manager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    TeamMemberComponent,
+    TeamLeaderComponent,
+    ManagerComponent
   ],
   imports: [
     BrowserModule,
